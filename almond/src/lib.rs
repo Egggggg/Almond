e1f45fdb0ac1b450226597df4a2ba33bca8d7e0e
Egggggg <mrhumanman2@gmail.com>
2022-08-20 16:25:51 -0400

    Add new almond crate for language

--- almond/src/lib.rs
+++ almond/src/lib.rs
@@ -1,100 +1,90 @@
-// use core::cmp::Ordering;
-use std::fmt::{self, Display};
+use logos::Logos;
 
-pub type BaseOutputGroup = bool;
-pub type VecOutputGroup = bool;
+#[derive(Logos, Debug, PartialEq)]
+pub enum Token {
+    // ===== general =====
+    #[regex(r"[a-zA-Z][\w_]*")]
+    Ident,
+    #[token("=")]
+    Assign,
+    #[token(";")]
+    End,
+    #[token("$")]
+    Scope,
+    #[token("$#")]
+    IScope,
+    #[token("import")]
+    Import,
+    #[token("as")]
+    As,
+    #[token(",")]
+    Comma,
+    #[token(".")]
+    Access,
+    #[error]
+    Error,
+    #[regex(r"[ \t\n\f]+", logos::skip)]
+    Skipped,
 
-#[derive(Clone, Debug)]
-pub enum BaseOutput {
-    Str(String),
-    Bool(bool),
-    Int(i32),
-    Float(f64),
-    Path(RefPath),
-}
+    // ===== containers =====
+    #[token("{")]
+    LCurly,
+    #[token("}")]
+    RCurly,
+    #[token("[")]
+    LSquare,
+    #[token("]")]
+    RSquare,
+    #[token("(")]
+    LParen,
+    #[token(")")]
+    RParen,
+    #[token("'")]
+    SQuote,
+    #[token(r#"""#)]
+    DQuote,
 
-#[derive(Clone, Debug)]
-pub struct VecOutput<T: Output> {
-    pub items: Vec<T>,
-}
+    // ===== logic =====
+    #[token("==")]
+    Equals,
+    #[token("<")]
+    Lt,
+    #[token(">")]
+    Gt,
+    #[token("<=")]
+    Lte,
+    #[token(">=")]
+    Gte,
+    #[token("&&")]
+    And,
+    #[token("||")]
+    Or,
+    #[token("!")]
+    Not,
 
-pub trait Output: Display {
-    type Group;
+    // ===== math =====
+    #[token("+")]
+    Add,
+    #[token("-")]
+    Sub,
+    #[token("*")]
+    Mul,
+    #[token("/")]
+    Div,
+    #[token("%")]
+    Mod,
+    #[token("**")]
+    Exp,
 
-    fn len(&self) -> usize {
-        0
-    }
-}
-
-#[derive(Clone, Debug)]
-pub struct RefPath {
-    pub parts: Vec<String>,
-}
-
-impl Output for BaseOutput {
-    type Group = BaseOutputGroup;
-
-    fn len(&self) -> usize {
-        match self {
-            BaseOutput::Str(e) => e.as_bytes().len(),
-            BaseOutput::Bool(e) => e.to_string().as_bytes().len(),
-            BaseOutput::Int(e) => e.to_string().as_bytes().len(),
-            BaseOutput::Float(e) => e.to_string().as_bytes().len(),
-            BaseOutput::Path(e) => e.to_string().as_bytes().len(),
-        }
-    }
-}
-
-impl Display for BaseOutput {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        let out = match self {
-            BaseOutput::Str(e) => return write!(f, "{}", e),
-            BaseOutput::Bool(e) => e.to_string(),
-            BaseOutput::Int(e) => e.to_string(),
-            BaseOutput::Float(e) => e.to_string(),
-            BaseOutput::Path(e) => e.to_string(),
-        };
-
-        write!(f, "{}", out)
-    }
-}
-
-impl<T: Output> Output for VecOutput<T> {
-    type Group = VecOutputGroup;
-
-    fn len(&self) -> usize {
-        self.items.len()
-    }
-}
-
-impl<T: Output> Display for VecOutput<T> {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        let mut counter = 0;
-        let mut out: String = "".to_owned();
-
-        for i in self.items.iter() {
-            out.push('/');
-            out.push_str(&i.to_string());
-            counter += 1;
-
-            if counter == 10 {
-                break;
-            }
-        }
-
-        write!(f, "{}", out)
-    }
-}
-
-impl Display for RefPath {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        let mut out: String = "".to_owned();
-
-        for i in self.parts.iter() {
-            out.push('/');
-            out.push_str(i.as_str())
-        }
-
-        write!(f, "{}", out)
-    }
+    // ===== literal =====
+    #[regex(r#""[^(\n)(\r\n)]*""#)]
+    String,
+    #[regex(r"\d[\d_]*")]
+    Int,
+    #[regex(r"(\d[\d_]*)?\.(\d[\d_]*)([eE]\d[\d_]*)?")]
+    Float,
+    #[token("true")]
+    True,
+    #[token("false")]
+    False,
 }
