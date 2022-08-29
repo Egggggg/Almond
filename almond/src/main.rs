74244ed6e6eac6c3f04bfb469c5333332485886e
Bee Clark <mrhumanman2@gmail.com>
2022-08-29 19:40:36 -0400

    More powerful testing input

--- almond/src/main.rs
+++ almond/src/main.rs
@@ -1,7 +1,7 @@
 use almond::parser;
 
 fn main() {
-	let input = r#"nice = !false cool = -923864"#;
+	let input = r#"nice = 12 + 39 * 43289 % 392 / 9 ** (12 + 4); cool = nice * (3 + nice);"#;
     let mut parser = parser::Parser::new(input);
 	
 	println!("{:#?}", parser);
